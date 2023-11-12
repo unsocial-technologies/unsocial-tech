699353ef92eb10b132095d8703f2135fd0903ade
shivang-stack <shivang1691401@gmail.com>
2023-11-12 16:06:41 +0530

    Second commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Navbar from '../components/Navabar'
+import Main from '../components/Main'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import Projects from '../components/Project'
+import Contact from '../components/Contact'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>UnSocial Tech</title>
+        <meta name="UnSocial Tech" content="We are best in business" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar/>
+      <Main/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+    </div>
+  )
+}
