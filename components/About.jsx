0c274029b089b5cf6634eaa79d097c0eaa539ef3
shivang-stack <shivang1691401@gmail.com>
2023-11-13 22:59:54 +0530

    about section updated

--- components/About.jsx
+++ components/About.jsx
@@ -3,34 +3,36 @@ import shivangp from '../public/assets/about/shivang.jpg'
 import tanayp from '../public/assets/about/tanay.png'
 import aryanp from '../public/assets/about/aryan.jpg'
 import TeamComponent from './TeamComponent';
+import Slider from 'react-slick';
 
 const About = () => {
+  
   const experiences = [
     {
         profilepic: shivangp,
         name: 'Shivang Yadav, Full Stack developer',
         linkedin: 'https://www.linkedin.com/in/shivangyadav16/',
         github:'https://github.com/Shivang-stack',
-        description: 'In my 2 years as a full stack developer in the node js react and next js space in startup space.I have gained a lot of experience and knowledge. I have worked on various projects, both big and small, and have learned a lot along the way.I have also gained a lot of experience in working with different teams and clients,and have learned how to manage and communicate with them effectively.I am also well-versed in the latest tools and technologies, and am always keen to learn new things.',
+        description: '',
       },
     {
         profilepic: tanayp,
         name: 'Tanay Jagnani, UI/UX designer And Frontend Developer',
         linkedin: 'https://www.linkedin.com/in/shivangyadav16/',
         github:'https://github.com/Shivang-stack',
-        description: 'Hi, I am Tanay Janani and I am a designer and front-end developer. I have worked on a variety of projects, from small websites to large enterprise applications. I am always looking to push my boundaries and learn new technologies.',
+        description: '',
     },
     {
         profilepic: aryanp,
         name: 'Aryan Agarwal, Backend developer',
         linkedin: 'https://www.linkedin.com/in/shivangyadav16/',
         github:'https://github.com/Shivang-stack',
-        description: 'My name is Aryan Agarwal and I am a backend developer with experience in Java Springboot.I have a strong educational background in business, which gives me a unique perspective on developing software solutions. I am always looking for ways to improve my skills and learn new technologies.',
+        description: '',
     },
   ];
   return (
     <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
-      <div className='max-w-[1240px] m-auto px-2 py-16'>
+      <div className='container mx-auto px-2 py-16'>
             <p className='text-xl pt-5 tracking-widest uppercase text-[#cfbaec]'>About Us</p>
             <h2 className=''>Our Team</h2>
             <div className='pt-5'>
--- components/TeamComponent.jsx
+++ components/TeamComponent.jsx
@@ -6,6 +6,17 @@ import 'slick-carousel/slick/slick-theme.css';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import Link from 'next/link';
 
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "none", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
+
 const TeamComponent = ({ teamData }) => {
   if (!teamData || teamData.length === 0) {
     return null; // or some default content or message
@@ -17,35 +28,41 @@ const TeamComponent = ({ teamData }) => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: <SampleNextArrow/>,
+    prevArrow: <SampleNextArrow/>
   };
 
   return (
-    <Slider {...settings}>
-      {teamData.map(({ profilepic, name, description , linkedin,github}, index) => (
-        <div key={index} className='p-6 shadow-xl rounded-xl backdrop-blur-sm bg-black/30'>
-          <div className="grid grid-cols-5 gap-4">
-            <div className='col-span-1'>
-              <Image
-                className='rounded'
-                src={profilepic}
-                width='250px'
-                height='250px'
-                alt='/'
-              />
-            </div>
-            <div className='col-span-4'>
-              <h3 className="text-lg font-bold">{name}</h3>
-              <p className="text-gray-300">{description}</p>
-              <div>
-                <div className='p-3 cursor-pointer'>
-                    <Link href={linkedin}>
-                        <FaLinkedinIn />
-                    </Link>
-                </div>
-                <div className='p-3 cursor-pointer'>
-                    <Link href={github}>
-                        <FaGithub />
-                    </Link>
+    <Slider {...settings} className="mt-7">
+      {teamData.map(({ profilepic, name, description, linkedin, github }, index) => (
+        <div key={index} className="mx-auto">
+          <div className=" backdrop-blur-sm p-6 rounded-lg bg-black/30">
+            <div className="flex items-center space-x-3">
+              <div className="flex-shrink-0">
+                <Image
+                  className="rounded-full"
+                  src={profilepic}
+                  width={80}
+                  height={80}
+                  alt=""
+                />
+              </div>
+              <div className="flex-1">
+                <h3 className="text-xl font-bold text-white">{name}</h3>
+                <p className="text-gray-400">{description}</p>
+                <div className="flex space-x-2 mt-4">
+                  <Link href={linkedin}>
+                    <a target="_blank" rel="noopener noreferrer" className="text-blue-500">
+                      <FaLinkedinIn />
+                    </a>
+                  </Link>
+                  <Link href={github}>
+                    <a target="_blank" rel="noopener noreferrer" className="text-gray-500">
+                      <FaGithub />
+                    </a>
+                  </Link>
                 </div>
               </div>
             </div>
